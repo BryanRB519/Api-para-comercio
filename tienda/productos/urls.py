ad6d649cfe50e1c36722e1349c6435ba49db6d84
Bryan Rojas <bryan519_puyol@hotmail.com>
2024-12-05 02:18:01 -0300

    Add files via upload

--- tienda/productos/urls.py
+++ tienda/productos/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-from .views import RegistrarVenta
+from django.contrib import admin
+from django.urls import path, include
+from productos.views import RegistrarVenta  # Asegúrate de importar la vista correctamente
 
 urlpatterns = [
-    path('api/productos/vender/', RegistrarVenta.as_view(), name='registrar_venta'),
+    path('admin/', admin.site.urls),
+    path('', RegistrarVenta.as_view(), name='registrar_venta'),  # Esta ruta es para la URL raíz
+    path('api/registrar-venta/', RegistrarVenta.as_view(), name='registrar_venta'),  # Ruta para API
+    path('productos/', include('productos.urls')),
 ]
-
